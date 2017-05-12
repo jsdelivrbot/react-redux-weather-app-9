98b3682a9a014eb543c6764109ab29f0cd36436c
Jonathan Ly <jonathanlyhv@gmail.com>
2017-05-12 19:51:36 +1000

    Created WeatherList and Chart components

---
+++ src/components/Chart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
+
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
+
+const Chart = ({ data, color, units }) => {
+  return (
+    <div>
+      <Sparklines height={120} width={180} data={data}>
+        <SparklinesLine color={color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>{average(data)} {units}</div>
+    </div>
+  );
+}
+
+export default Chart;
---
+++ src/containers/WeatherList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Chart from '../components/Chart';
+
+class WeatherList extends React.Component {
+  renderWeather(cityData) {
+    const { name } = cityData.city;
+    const temp = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td><Chart data={temp} color="orange" units="C" /></td>
+        <td><Chart data={pressure} color="red" units="hPa" /></td>
+        <td><Chart data={humidity} color="blue" units="%" /></td>
+      </tr>
+    );
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temperature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
