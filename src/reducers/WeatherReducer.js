9f45eab3cb414e1523a26bdbef6d3b981f503aa5
Jonathan Ly <jonathanlyhv@gmail.com>
2017-05-12 19:50:32 +1000

    Weather reducer updates state on each addition

--- src/reducers/WeatherReducer.js
+++ src/reducers/WeatherReducer.js
@@ -1,4 +1,10 @@
-export default function(state = null, action) {
-  console.log(action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // return state.concat([ action.payload.data ]);  // inserts to end of array
+      return [ action.payload.data, ...state ]; // adds to start of array
+  }
   return state;
 }
